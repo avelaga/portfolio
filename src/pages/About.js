7369d17e442d14ba59d6d980761703757ea01c0e
Abhi Velaga <abhinav.velaga@gmail.com>
2021-12-26 11:42:32 -0600

    updated home and about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,83 +1,40 @@
-import React, { Component } from "react";
-
+import React from "react";
 import Navbar from '../layout/Navbar'
 import MediaQuery from 'react-responsive'
 import me1 from "../../dist/images/home/room.jpg";
 import me2 from "../../dist/images/home/mirror.jpg";
 import resume from "../../dist/images/Resume.pdf";
-import './pages.css';
-
 import { LazyLoadImage } from 'react-lazy-load-image-component';
+import './pages.css';
 import 'react-lazy-load-image-component/src/effects/opacity.css';
 
-export class About extends Component {
+const mobileImg = {
+  width: '100vw'
+};
 
-  getWidth(image) {
-    var img = new Image();
-    img.onload = function () {
-      return this.width;
-    }
-    img.src = image;
-  }
+export default function About() {
+  return <div>
+    <MediaQuery minDeviceWidth={500}><Navbar activeLink={"ABOUT"} mobile={false} /></MediaQuery>
+    <MediaQuery maxDeviceWidth={500}><Navbar activeLink={"ABOUT"} mobile={true} /></MediaQuery>
 
-  getHeight(image) {
-    var img = new Image();
-    img.onload = function () {
-      return this.height;
-    }
-    img.src = image;
-  }
-
-  render() {
-    return <div>
-      <MediaQuery minDeviceWidth={500}><Navbar activeLink={"ABOUT"} mobile={false} /></MediaQuery>
-      <MediaQuery maxDeviceWidth={500}><Navbar activeLink={"ABOUT"} mobile={true} /></MediaQuery>
-
-      {/* desktop  */}
-      <MediaQuery minDeviceWidth={500}>
-        <div className="page">
-          <div className="appear">
-            <div className="about-top">
-              <LazyLoadImage src={me1} className="about-top-img" effect="opacity" height={this.getHeight(me1)} width={this.getWidth(me1)} />
-              <div className="about-second-text">
-              I'm a software developer, musician, and artist based in Austin, TX where I studied both Computer Science and Studio Art at <ut>The University of Texas at Austin</ut> and currently work as a software engineer at <processing>Visa</processing>.
-                <br />
-                <br />
-                I released my first solo album at the age of 16 and a second at 18, my first photo collection at the age of 19, played my first official SXSW showcase at the age of 20, built my first interactive art installation for Fortress Festival a month later, and had my photography featured on the cover of Spark Magazine.
-            </div>
-            </div>
-            <div className="about-bottom">
-              <div className="about-bottom-left-text">
-                Web design and development by Abhi Velaga
-            </div>
-              <LazyLoadImage src={me2} className="about-bottom-img" effect="opacity" height={this.getHeight(me2)} width={this.getWidth(me2)} />
-              <div className="about-bottom-text">
-                <a href="mailto:abhinav.velaga@utexas.edu">abhinav.velaga@utexas.edu</a>
-                <br />
-                <a href="https://www.instagram.com/abhi.velaga/" target='_blank'>@abhi.velaga</a>
-                <br />
-                <a href="https://www.instagram.com/abhi.film/" target='_blank'>@abhi.film</a>
-                <br />
-                <a href={resume} target='_blank'>[ resume ]</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </MediaQuery>
-
-      {/* mobile  */}
-      <MediaQuery maxDeviceWidth={500}>
-        <div className="page">
-          <div className="appear">
-            <div className="about-first-text">
-            I'm a software developer, musician, and artist based in Austin, TX where I studied both Computer Science and Studio Art at <ut>The University of Texas at Austin</ut> and currently work as a software engineer at <processing>Visa</processing>.
-            </div>
-            <LazyLoadImage effect="opacity" src={me1} className="about-top-img" height={this.getHeight(me1)} width={this.getWidth(me1)} />
+    {/* desktop  */}
+    <MediaQuery minDeviceWidth={500}>
+      <div className="page">
+        <div className="appear">
+          <div className="about-top">
+            <LazyLoadImage src={me1} className="about-top-img" effect="opacity"  />
             <div className="about-second-text">
-            I released my first solo album at the age of 16 and a second at 18, my first photo collection at the age of 19, played my first official SXSW showcase at the age of 20, built my first interactive art installation for Fortress Festival a month later, and had my photography featured on the cover of Spark Magazine.
-            </div>
-            <LazyLoadImage effect="opacity" src={me2} className="about-bottom-img" height={this.getHeight(me2)} width={this.getWidth(me2)} />
-
+            I'm a software developer, musician, and artist based in Austin, TX where I studied both Computer Science and Studio Art at <ut>The University of Texas at Austin</ut> and currently work as a software engineer at <processing>Visa</processing>.
+              <br />
+              <br />
+              I released my first solo album at the age of 16 and a second at 18, my first photo collection at the age of 19, played my first official SXSW showcase at the age of 20, built my first interactive art installation for Fortress Festival a month later, and had my photography featured on the cover of Spark Magazine.
+          </div>
+          </div>
+          <div className="about-bottom">
+            <div className="about-bottom-left-text">
+              Web design and development by Abhi Velaga
+          </div>
+            <LazyLoadImage src={me2} className="about-bottom-img" effect="opacity"  />
             <div className="about-bottom-text">
               <a href="mailto:abhinav.velaga@utexas.edu">abhinav.velaga@utexas.edu</a>
               <br />
@@ -87,19 +44,39 @@ export class About extends Component {
               <br />
               <a href={resume} target='_blank'>[ resume ]</a>
             </div>
-
-            <div className="about-bottom-left-text">
-              Web design and development by Abhi Velaga
-            </div>
           </div>
         </div>
-      </MediaQuery>
-    </div>;
-  }
-}
+      </div>
+    </MediaQuery>
 
-const mobileImg = {
-  width: '100vw'
-}
+    {/* mobile  */}
+    <MediaQuery maxDeviceWidth={500}>
+      <div className="page">
+        <div className="appear">
+          <div className="about-first-text">
+          I'm a software developer, musician, and artist based in Austin, TX where I studied both Computer Science and Studio Art at <ut>The University of Texas at Austin</ut> and currently work as a software engineer at <processing>Visa</processing>.
+          </div>
+          <LazyLoadImage effect="opacity" src={me1} className="about-top-img"  />
+          <div className="about-second-text">
+          I released my first solo album at the age of 16 and a second at 18, my first photo collection at the age of 19, played my first official SXSW showcase at the age of 20, built my first interactive art installation for Fortress Festival a month later, and had my photography featured on the cover of Spark Magazine.
+          </div>
+          <LazyLoadImage effect="opacity" src={me2} className="about-bottom-img"  />
 
-export default About;
+          <div className="about-bottom-text">
+            <a href="mailto:abhinav.velaga@utexas.edu">abhinav.velaga@utexas.edu</a>
+            <br />
+            <a href="https://www.instagram.com/abhi.velaga/" target='_blank'>@abhi.velaga</a>
+            <br />
+            <a href="https://www.instagram.com/abhi.film/" target='_blank'>@abhi.film</a>
+            <br />
+            <a href={resume} target='_blank'>[ resume ]</a>
+          </div>
+
+          <div className="about-bottom-left-text">
+            Web design and development by Abhi Velaga
+          </div>
+        </div>
+      </div>
+    </MediaQuery>
+  </div>;
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
-import Navbar from '../layout/Navbar'
-import MediaQuery from 'react-responsive'
+import React, { useEffect } from 'react';
+import Navbar from '../layout/Navbar';
+import MediaQuery from 'react-responsive';
 import room from "../../dist/images/home/room.jpg";
 import deyoung from "../../dist/images/home/deyoung-desktop.jpg";
 import gym from "../../dist/images/photos/portfolio/p1.jpg";
@@ -13,35 +13,31 @@ import drummingMobile from "../../dist/images/music/mjf/drummingmobile.mp4";
 
 const images = [
   gym, deyoung, ania2, room, spark, house, ania1
-]
+];
 
-export class Home extends Component {
+export default function Home() {
   // easter egg :)
-  componentDidMount(){
+  useEffect(() => {
     console.log(" _____                                   \r\n|_   _|                                  \r\n  | |                                    \r\n  | |                                    \r\n _| |_                                   \r\n \\___\/                                   \r\n                                         \r\n                                         \r\n                                         \r\n                                         \r\n ___  ___  ___                           \r\n\/ __|\/ _ \\\/ _ \\                          \r\n\\__ \\  __\/  __\/                          \r\n|___\/\\___|\\___|                          \r\n                                         \r\n                                         \r\n                                         \r\n                                         \r\n _   _  ___  _   _                       \r\n| | | |\/ _ \\| | | |                      \r\n| |_| | (_) | |_| |                      \r\n \\__, |\\___\/ \\__,_|                      \r\n  __\/ |                                  \r\n |___\/                                   \r\n                      _    _             \r\n                     | |  (_)            \r\n ___ _ __   ___  __ _| | ___ _ __   __ _ \r\n\/ __| \'_ \\ \/ _ \\\/ _` | |\/ \/ | \'_ \\ \/ _` |\r\n\\__ \\ | | |  __\/ (_| |   <| | | | | (_| |\r\n|___\/_| |_|\\___|\\__,_|_|\\_\\_|_| |_|\\__, |\r\n                                    __\/ |\r\n                                   |___\/ \r\n _                                       \r\n(_)                                      \r\n _ _ __                                  \r\n| | \'_ \\                                 \r\n| | | | |                                \r\n|_|_| |_|                                \r\n                                         \r\n                                         \r\n _   _                                   \r\n| | | |                                  \r\n| |_| |__   ___                          \r\n| __| \'_ \\ \/ _ \\                         \r\n| |_| | | |  __\/                         \r\n \\__|_| |_|\\___|                         \r\n                                         \r\n                                         \r\n                           _             \r\n                          | |            \r\n  ___ ___  _ __  ___  ___ | | ___        \r\n \/ __\/ _ \\| \'_ \\\/ __|\/ _ \\| |\/ _ \\       \r\n| (_| (_) | | | \\__ \\ (_) | |  __\/       \r\n \\___\\___\/|_| |_|___\/\\___\/|_|\\___|       \r\n                                         \r\n                                         \r\n  ______                                 \r\n _|  _  \\                                \r\n(_) | | |                                \r\n  | | | |                                \r\n _| |\/ \/                                 \r\n(_)___\/                                  \r\n                                         \r\n                                         ");
-  }
+  });
 
-  render() {
-    return (
-      <div className="appear">
-        <MediaQuery minDeviceWidth={500}><Navbar activeLink={"HOME"} mobile={false} /></MediaQuery>
-        <MediaQuery maxDeviceWidth={500}><Navbar activeLink={"HOME"} mobile={true} /></MediaQuery>
+  return (
+    <div className="appear">
+      <MediaQuery minDeviceWidth={500}><Navbar activeLink={"HOME"} mobile={false} /></MediaQuery>
+      <MediaQuery maxDeviceWidth={500}><Navbar activeLink={"HOME"} mobile={true} /></MediaQuery>
         <div className="fadein">
-          <video playsInline autoPlay muted loop className="f1">
-            <MediaQuery minDeviceWidth={500}><source src={drumming} type="video/mp4" /></MediaQuery>
-            <MediaQuery maxDeviceWidth={500}><source src={drummingMobile} type="video/mp4" /></MediaQuery>
-          </video>
-          <img src={images[0]} className="f2" />
-          <img src={images[1]} className="f3" />
-          <img src={images[2]} className="f4" />
-          <img src={images[3]} className="f5" />
-          <img src={images[4]} className="f6" />
-          <img src={images[5]} className="f7" />
-          <img src={images[6]} className="f8" />
-        </div>
+        <video playsInline autoPlay muted loop className="f1">
+          <MediaQuery minDeviceWidth={500}><source src={drumming} type="video/mp4" /></MediaQuery>
+          <MediaQuery maxDeviceWidth={500}><source src={drummingMobile} type="video/mp4" /></MediaQuery>
+        </video>
+        <img src={images[0]} className="f2" />
+        <img src={images[1]} className="f3" />
+        <img src={images[2]} className="f4" />
+        <img src={images[3]} className="f5" />
+        <img src={images[4]} className="f6" />
+        <img src={images[5]} className="f7" />
+        <img src={images[6]} className="f8" />
       </div>
-    )
-  }
+    </div>
+  )
 }
-
-export default Home
