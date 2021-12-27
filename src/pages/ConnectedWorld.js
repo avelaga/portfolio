63e296aa4890e75005c932b941ec531ab1c3b265
Abhi Velaga <abhinav.velaga@gmail.com>
2021-12-26 22:08:41 -0600

    updated connected world to functional

--- src/pages/ConnectedWorld.js
+++ src/pages/ConnectedWorld.js
@@ -1,18 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Navbar from '../layout/Navbar';
 import MediaQuery from 'react-responsive';
 import ScriptTag from 'react-script-tag';
 
-export class ConnectedWorld extends Component {
-  render() {
-    return (
-      <div className="appear">
-        <MediaQuery minDeviceWidth={500}><Navbar activeLink={"HOME"} mobile={false} /></MediaQuery>
-        <MediaQuery maxDeviceWidth={500}><Navbar activeLink={"HOME"} mobile={true} /></MediaQuery>
-        <ScriptTag type="text/javascript" src="sketch.js" />
-      </div>
-    )
-  }
-}
-
-export default ConnectedWorld
+export default function ConnectedWorld() {
+  return (
+    <div className="appear">
+      <MediaQuery minDeviceWidth={500}><Navbar activeLink={"HOME"} mobile={false} /></MediaQuery>
+      <MediaQuery maxDeviceWidth={500}><Navbar activeLink={"HOME"} mobile={true} /></MediaQuery>
+      <ScriptTag type="text/javascript" src="sketch.js" />
+    </div>
+  )
+};
