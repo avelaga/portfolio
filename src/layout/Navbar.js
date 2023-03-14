283d4c43c1e3b5070235046c2e17a013cffbc730
Abhi Velaga <abhinav.velaga@gmail.com>
2023-03-13 20:01:06 -0500

    add menu highlight, fixed external link hover animation

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,25 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MediaQuery from 'react-responsive'
 import './layout.scss'
 
-export default function Navbar(){
+export default function Navbar({activeLink}){
+
+  const [collapsed, setCollapsed] = useState(true);
+
+  const activeStyle = {
+    backgroundColor: 'black'
+  };
+
   return <div className='navbar-container'>
     <div className='navbar'>
       <div className='nav-title'>Abhi Velaga</div>
         <div className='subtitle'>austin, tx</div>
         
         <div className='link-container'>
-        <a href="/about"><div className='link'>ABOUT</div></a>
-        <a href="/photo"><div className='link'>PHOTO</div></a>
+        <a href="/about"><div className='link'  style={activeLink === "ABOUT" ? activeStyle : {}}>ABOUT</div></a>
+        <a href="/photo"><div className='link' style={activeLink === "PHOTO" ? activeStyle : {}}>PHOTO</div></a>
           <div className='sublink-container'>
-          <a href="/portfolio"><div className='sublink'>portfolio</div></a>
-          <a href="/miamivice"><div className='sublink'>miami vice cocaine dream</div></a>
-          <a href="/uservoid"><div className='sublink'>user void</div></a>
-          <a href="/published"><div className='last-sublink'>published work</div></a>
+          <a href="/portfolio"><div className='sublink' style={activeLink === "PORTFOLIO" ? activeStyle : {}}>portfolio</div></a>
+          <a href="/miamivice"><div className='sublink' style={activeLink === "MIAMI" ? activeStyle : {}}>miami vice cocaine dream</div></a>
+          <a href="/uservoid"><div className='sublink' style={activeLink === "USERVOID" ? activeStyle : {}}>user void</div></a>
+          <a href="/published"><div className='last-sublink' style={activeLink === "PUBLISHED" ? activeStyle : {}}>published work</div></a>
           </div>
-          <a href="/music"><div className='resume-link'>MUSIC</div></a>
-          <a href="/software"><div className='link'>SOFTWARE</div></a>
-          <a href="/hi"><div className='last-link'>SAY HI</div></a>
+          <a href="/music"><div className='resume-link' style={activeLink === "MUSIC" ? activeStyle : {}}>MUSIC</div></a>
+          <a href="/software"><div className='link' style={activeLink === "SOFTWARE" ? activeStyle : {}}>SOFTWARE</div></a>
+          <a href="/hi"><div className='last-link' style={activeLink === "HI" ? activeStyle : {}}>SAY HI</div></a>
         </div>
         <div className='external-link' />
         <a href="https://open.spotify.com/artist/7JCVu8JTFAFrHqnYnGVPhi?si=wAs1clwUTiqOqS8e8RUJqg" target="_blank">
