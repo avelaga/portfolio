24fb6a7be5996b1c5d79b4d3298c5b26ef5df572
Abhi Velaga <abhinav.velaga@gmail.com>
2023-03-25 00:34:28 -0500

    music desktop roughly done

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -1,20 +1,19 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
-import MediaQuery from 'react-responsive';
 import Navbar from '../layout/Navbar';
 import YouTube from 'react-youtube';
-
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import 'react-lazy-load-image-component/src/effects/opacity.css';
-
 import './pages.scss';
-import styles from './Music.module.scss';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import MediaQuery from 'react-responsive';
+import 'react-lazy-load-image-component/src/effects/opacity.css';
 
 import drums from '../../dist/images/music/indulgent/drums.jpg';
 import guitar1 from '../../dist/images/music/indulgent/guitar1.jpg';
 import guitar2 from '../../dist/images/music/indulgent/guitar2.jpg';
-import indulgentCover from '../../dist/images/music/indulgent/cover.jpg';
 import mebed from '../../dist/images/music/indulgent/mebed.jpg';
+import { indulgentReleases } from './musicImports.js';
+
+import caladan from '../../dist/images/music/caladan/caladan.jpg';
+import caladanGroup from '../../dist/images/music/caladan/group.jpg';
 
 import mjf from '../../dist/images/music/mjf/mjf.jpg';
 import mjfStudio from '../../dist/images/music/mjf/studio.jpg';
@@ -22,144 +21,93 @@ import mjfCover from '../../dist/images/music/mjf/cover.jpg';
 
 import sd1 from '../../dist/images/music/sd/sd1.jpg';
 import sd2 from '../../dist/images/music/sd/sd2.jpg';
+import fallingKid from '../../dist/images/music/sd/fallingKid.jpg';
+
 
 export default function Music() {
+    return <div>
+        <MediaQuery minDeviceWidth={500}><Navbar activeLink={"MUSIC"} mobile={false} /></MediaQuery>
+        <MediaQuery maxDeviceWidth={500}><Navbar activeLink={"MUSIC"} mobile={true} /></MediaQuery>
+        <div className="appear page gallery">
+            <div className="section-title">INDULGENT</div>
+            <div className="section-subtitle">guitar, vocals, drums, bass</div>
+            <div className="section-year">2015 - present</div>
+            <img src={mebed} className="music-full" />
+            <div className="music-description">I perform all vocals, drums, guitars, and bass myself for my solo project, Indulgent, and released my recent album, <a href="https://open.spotify.com/album/2d7c0N7uNdi3jfdsTWeZNV?si=K98wvkQmQd6uMvHFrQt2ng">No Drive Will Take Me Home</a>, in 2018 and my debut album, <a href="https://open.spotify.com/album/67RH1N86fW4hIA10m3DhFl?si=Hl4dag6_QhSyX3f4Zv0mhg">Toxic</a>, in 2015.</div>
+            <img src={guitar1} className="music-half" />
+            <img src={guitar2} className="music-half" />
+            <div className="music-releases">
+                {indulgentReleases.map((releaseObj) =>
+                    <div className="music-release">
+                        <a href={releaseObj.link}>
+                            <img src={releaseObj.cover} className="music-cover" />
+                            {releaseObj.title}
+                            <br />
+                            {releaseObj.year}
+                        </a>
+                    </div>
+                )}
+            </div>
+            <div className="line" />
 
-  const getHeight = function(image) {
-    var img = new Image();
-    img.onload = function () {
-      return this.height;
-    }
-    img.src = image;
-  }
+            <div className="section-title">CALADAN</div>
+            <div className="section-subtitle">drums</div>
+            <div className="section-year">2022 - present</div>
+            <img src={caladan} className="music-full" />
+            <div className="music-description">WHAT DO I SAY ABOUT CALDAN</div>
+            <div className="music-video">
+                <YouTube videoId="VQsZdVG8A8I" className="youtube-half" />
+                hole in the wall <br />
+                1.7.23
+            </div>
+            <div className="music-video">
+                <YouTube videoId="hQgL_IebL_s" className="youtube-half" />
+                woodshed <br /> 12.17.22
+            </div>
+            <img src={caladanGroup} className="music-half" />
 
-  return (
-  <div>
-    <Navbar activeLink={"MUSIC"} />
-    <div className="wrapper appear">
-        {/* indulgent  */}
-        <div className="parallax">
-          <img src={mebed} className="parallax-img" />
-          <div className="parallax-title-black">INDULGENT</div>
+            <div className="line" />
+
+            <div className="section-title">SHIVERING DEMONS</div>
+            <div className="section-subtitle">drums</div>
+            <div className="section-year">2022 - 2023</div>
+            <img src={sd1} className="music-full" />
+            <div className="music-description">WHAT DO I SAY ABOUT SHIVERING DMEONSSS</div>
+            <img src={fallingKid} className="music-half" />
+            <div className="music-video">
+                <YouTube videoId="1ap9ObDNqAU" className="youtube-half" />
+                far out lounge <br />  1.13.23
+            </div>
+            <div className="music-video">
+                <YouTube videoId="08FnNPD-Eng" className="youtube-half" />
+                11.11.22 <br /> rio market
+            </div>
+            <div className="music-video">
+                <YouTube videoId="6mEqHssj27Y" className="youtube-half" />
+                pearl st. co-op <br /> 10.28.22
+            </div>
+
+            <div className="line" />
+
+            <div className="section-title">MERRY JANE & THE FONDAS</div>
+            <div className="section-subtitle">drums</div>
+            <div className="section-year">2018 - 2019</div>
+            <img src={mjf} className="music-full" />
+            <div className="music-description">
+                As the drummer for Merry Jane & The Fondas from 2018 to 2019, I recorded a <a href="https://open.spotify.com/album/097Hlqmh6mqEv6GJbxf61L?si=NO5UrK5mTlymBRe4vKErNw">studio EP</a> with renowned guitarist and songwriter, <a href="https://en.wikipedia.org/wiki/Jackie_Venson">Jackie Venson</a>, and also played an <a href="https://schedule.sxsw.com/2019/artists/2008465">official SXSW showcase</a>.
+            </div>
+            <img src={mjfStudio} className="music-half" />
+            <YouTube videoId="XXMGt_N6Mc0" className="youtube-half" />
+            <div className="music-releases">
+                <div className="music-release">
+                    <a href="https://open.spotify.com/album/097Hlqmh6mqEv6GJbxf61L?si=NO5UrK5mTlymBRe4vKErNw">
+                        <img src={mjfCover} className="music-cover" />
+                        Pulls You Under
+                        <br />
+                        2019
+                    </a>
+                </div>
+            </div>
         </div>
-
-        <div className="indulgent-grid" >
-          <LazyLoadImage effect="opacity" src={guitar1} className="indulgent-grid-img" width="100%" height={getHeight(drums)} />
-          <LazyLoadImage effect="opacity" src={guitar2} className="indulgent-grid-img" width="100%" height={getHeight(drums)} />
-          <div className="flex indulgent-grid-text">
-            <div className="music-text">
-              I perform all vocals, drums, guitars, and bass myself for my solo project, Indulgent, and released my recent album, <a href="https://open.spotify.com/album/2d7c0N7uNdi3jfdsTWeZNV?si=K98wvkQmQd6uMvHFrQt2ng">No Drive Will Take Me Home</a>, in 2018 and my debut album, <a href="https://open.spotify.com/album/67RH1N86fW4hIA10m3DhFl?si=Hl4dag6_QhSyX3f4Zv0mhg">Toxic</a>, in 2015.
-            </div>
-          </div>
-        </div>
-
-        {/* desktop  */}
-        <MediaQuery minDeviceWidth={500}>
-          <div className="flex cover-container">
-            <div className="indulgent-cover-info flex" >
-              <div className="cover-title" >No Drive Will Take Me Home</div>
-              <div className="cover-artist" >Indulgent</div>
-              <div className="indulgent-buttons cover-buttons flex">
-                <Button variant="outline-dark"><a href="https://open.spotify.com/album/2d7c0N7uNdi3jfdsTWeZNV?si=wxw93aEFTfGDUXMgTjEzsg" target="_blank">SPOTIFY</a></Button>
-                <Button variant="outline-dark"><a href="https://music.apple.com/us/album/no-drive-will-take-me-home/1429049796" target="_blank">APPLE MUSIC</a></Button>
-                <Button variant="outline-dark"><a href="https://play.google.com/store/music/album/Indulgent_No_Drive_Will_Take_Me_Home?id=Bcwfm3gxejnt5fvkv2hlhxgxdei" target="_blank">GOOGLE PLAY</a></Button></div>
-              {/* <div>Guitar/Drums/Vocals/Bass performed by Abhi Velaga</div> */}
-              <div>Released 2018</div>
-            </div>
-            <LazyLoadImage effect="opacity" src={indulgentCover} className="music-cover" height={getHeight(indulgentCover)} />
-          </div>
-        </MediaQuery>
-
-        {/* mobile  */}
-        <MediaQuery maxDeviceWidth={500}>
-          <div className="flex music-mobile">
-            <LazyLoadImage effect="opacity" src={indulgentCover} className="music-cover" height={getHeight(indulgentCover)} />
-            <div className="cover-title" >No Drive Will Take Me Home</div>
-            <div className="cover-artist" >Indulgent</div>
-            <div className="cover-buttons flex">
-              <Button variant="outline-dark"><a href="https://open.spotify.com/album/2d7c0N7uNdi3jfdsTWeZNV?si=wxw93aEFTfGDUXMgTjEzsg" target="_blank">SPOTIFY</a></Button>
-              <Button variant="outline-dark"><a href="https://music.apple.com/us/album/no-drive-will-take-me-home/1429049796" target="_blank">APPLE MUSIC</a></Button>
-              <Button variant="outline-dark"><a href="https://play.google.com/store/music/album/Indulgent_No_Drive_Will_Take_Me_Home?id=Bcwfm3gxejnt5fvkv2hlhxgxdei" target="_blank">GOOGLE PLAY</a>
-              </Button>
-            </div>
-            <div className="cover-extra">
-              <div>Guitar/Drums/Vocals/Bass performed by Abhi Velaga</div>
-              <div>Released 2018</div>
-            </div>
-          </div>
-        </MediaQuery>
-
-        {/* sd  */}
-        <div className="parallax">
-          <img src={sd1} className="parallax-img" />
-          <div className="parallax-title-white">SHIVERING DEMONS</div>
-        </div>
-
-        <YouTube videoId="gE3zPWgq4Ro" className="youtube" />
-
-        {/* mjf  */}
-        <div className="parallax">
-          <img src={mjf} className="parallax-img" />
-          <div className="parallax-title-white">MERRY JANE & THE FONDAS</div>
-        </div>
-
-        {/* desktop  */}
-        <MediaQuery minDeviceWidth={500}>
-          <div className="flex mjf-info">
-            <div className="mjf-description">
-             As the drummer for Merry Jane & The Fondas, I recorded a <a href="https://open.spotify.com/album/097Hlqmh6mqEv6GJbxf61L?si=NO5UrK5mTlymBRe4vKErNw">studio EP</a> that was produced by Jackie Venson and played an <a href="https://schedule.sxsw.com/2019/artists/2008465">official SXSW showcase</a>.
-          </div>
-            <LazyLoadImage effect="opacity" src={mjfStudio} className="mjf-studio" height={getHeight(mjfStudio)} />
-          </div>
-          <div className="flex cover-container">
-            <LazyLoadImage effect="opacity" src={mjfCover} className="music-cover" height={getHeight(mjfCover)} />
-            <div className="mjf-cover-info" >
-              <div className="cover-title" >Pulls You Under</div>
-              <div className="cover-artist" >Merry Jane & The Fondas</div>
-              <div className="mjf-buttons cover-buttons flex">
-                <Button variant="outline-dark"><a href="https://open.spotify.com/album/097Hlqmh6mqEv6GJbxf61L?si=NO5UrK5mTlymBRe4vKErNw" target="_blank">SPOTIFY</a></Button>
-                <Button variant="outline-dark"><a href="https://music.apple.com/us/album/pulls-you-under-ep/1454982478" target="_blank">APPLE MUSIC</a></Button>
-                <Button variant="outline-dark"><a href="https://play.google.com/store/music/album/Merry_Jane_the_Fondas_Pulls_You_Under?id=Bbp7z4p3isxbxsqv4oymqx7ndt4" target="_blank">GOOGLE PLAY</a></Button>
-              </div>
-              <div>Guitar/Vocals - Ally Brown</div>
-              <div>Lead Guitar/Backing Vocals - Serenity Autumn</div>
-              <div>Bass - Johnny Young</div>
-              <div>Drums - Abhi Velaga</div>
-              <div>Produced by Jackie Venson</div>
-              <br/>
-              <div>Released 2019</div>
-            </div>
-          </div>
-        </MediaQuery>
-
-        {/* mobile  */}
-        <MediaQuery maxDeviceWidth={500}>
-          <div className="flex music-mobile">
-            <div className="mjf-description">
-            As the drummer for Merry Jane & The Fondas from 2018 to 2019, I recorded a <a href="https://open.spotify.com/album/097Hlqmh6mqEv6GJbxf61L?si=NO5UrK5mTlymBRe4vKErNw">studio EP</a> with renowned guitarist and songwriter, <a href="https://en.wikipedia.org/wiki/Jackie_Venson">Jackie Venson</a>, and also played an <a href="https://schedule.sxsw.com/2019/artists/2008465">official SXSW showcase</a>.
-          </div>
-            <LazyLoadImage effect="opacity" src={mjfStudio} className="mjf-studio" height={getHeight(mjfStudio)} />
-            <LazyLoadImage effect="opacity" src={mjfCover} className="music-cover" height={getHeight(mjfCover)} />
-            <div className="cover-title" >Pulls You Under</div>
-            <div className="cover-artist" >Merry Jane & The Fondas</div>
-            <div className="cover-buttons flex">
-              <Button variant="outline-dark"><a href="https://open.spotify.com/album/097Hlqmh6mqEv6GJbxf61L?si=NO5UrK5mTlymBRe4vKErNw" target="_blank">SPOTIFY</a></Button>
-              <Button variant="outline-dark"><a href="https://music.apple.com/us/album/pulls-you-under-ep/1454982478" target="_blank">APPLE MUSIC</a></Button>
-              <Button variant="outline-dark"><a href="https://play.google.com/store/music/album/Merry_Jane_the_Fondas_Pulls_You_Under?id=Bbp7z4p3isxbxsqv4oymqx7ndt4" target="_blank">GOOGLE PLAY</a></Button>
-            </div>
-            <div className="cover-extra">
-              <div>Guitar/Vocals - Allyb</div>
-              <div>Lead Guitar/Backing Vocals - Serenity Autumn</div>
-              <div>Bass - Johnny Young</div>
-              <div>Drums - Abhi Velaga</div>
-              <div>Produced by Jackie Venson</div>
-              <div>Released 2019</div>
-            </div>
-          </div>
-        </MediaQuery>
-
-        <YouTube videoId="XXMGt_N6Mc0" className="youtube" />
-      </div>
-  </div>
-  );
-};
+    </div>
+}
---
+++ src/pages/musicImports.js
@@ -0,0 +1,73 @@
+import fourAm from '../../dist/images/music/indulgent/4am.jpg';
+// import letMeBe from '../../dist/images/music/indulgent/letMeBe.jpg';
+import toxic from '../../dist/images/music/indulgent/toxic.jpg';
+// import strippedDown from '../../dist/images/music/indulgent/strippedDown.jpg';
+import pleasureCruise from '../../dist/images/music/indulgent/pleasureCruise.jpg';
+import bleachYourHeart from '../../dist/images/music/indulgent/bleachYourHeart.jpg';
+import noDrive from '../../dist/images/music/indulgent/noDrive.jpg';
+import breakMyFall from '../../dist/images/music/indulgent/breakMyFall.jpg';
+import noOneKnows from '../../dist/images/music/indulgent/noOneKnows.jpg';
+import rebound from '../../dist/images/music/indulgent/rebound.jpg';
+
+export const indulgentReleases = [
+    {
+        cover: rebound,
+        title: "rebound - single",
+        year: 2022,
+        link: "https://open.spotify.com/album/4L6W0l9LPQhK441vkdfyiN?si=9T00ON31RqyT41o5B9nXzg"
+    },
+    {
+        cover: noOneKnows,
+        title: "no one knows - single",
+        year: 2022,
+        link: "https://open.spotify.com/album/6qJYfETeGcK4YTfgqjd74o?si=YHOZ7glASgii2Ys2D5qpMg"
+    },
+    {
+        cover: breakMyFall,
+        title: "break my fall - single",
+        year: 2022,
+        link: "https://open.spotify.com/album/5uWLGQTp9h1zTh2N55NYZR?si=6YBVkmqWReOPhpBXm3lpEw"
+    },
+    {
+        cover: noDrive,
+        title: "no drive will take me home",
+        year: 2018,
+        link: "https://open.spotify.com/album/2d7c0N7uNdi3jfdsTWeZNV?si=8k7F0AKVRBufzT6zhR4zPg"
+    },
+    {
+        cover: bleachYourHeart,
+        title: "bleach your heart - single",
+        year: 2018,
+        link: "https://open.spotify.com/album/1Bm4qwkp3SGQ4qaF1bmhH6?si=NTVpWxbHS-Cz-2gEkqcG_Q"
+    },
+    {
+        cover: pleasureCruise,
+        title: "pleasure cruise - single",
+        year: 2017,
+        link: "https://open.spotify.com/album/3sDC6afPmRBgT4PDnGruoB?si=CdLhyM9HSNWq_TelGai5AA"
+    },
+        // {
+    //     cover: strippedDown,
+    //     title: "stripped down - EP",
+    //     year: 2016,
+    //     link: ""
+    // },
+    {
+        cover: toxic,
+        title: "toxic",
+        year: 2015,
+        link: "https://open.spotify.com/album/67RH1N86fW4hIA10m3DhFl?si=luPqJ3hCTfCNZLzd8IPJSQ"
+    },
+        // {
+    //     cover: letMeBe,
+    //     title: "let me be",
+    //     year: 2016,
+    //     link: ""
+    // },
+    {
+        cover: fourAm,
+        title: "4am - single",
+        year: 2015,
+        link: "https://open.spotify.com/album/5yPH4IFWFQWJIGM9eiOYPX?si=izE6huAaTPqPOqumHKF70g"
+    }
+]
