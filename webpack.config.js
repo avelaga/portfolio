81358d71b00f0704a07fb8835b4f9ef100a286cd
Abhi Velaga <abhinav.velaga@gmail.com>
2026-01-14 12:05:45 -0600

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,63 @@
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
+
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
+
   module: {
     rules: [
       {
-        test: /.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
-        loader: "style-loader!css-loader"
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader",
-        ],
-      }, {
-        test: /\.(jpe?g|png|pdf|gif|woff|woff2|eot|ttf|otf|svg)(\?[a-z0-9=.]+)?$/,
-        loader: 'url-loader?limit=100000'
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
-      { test: /\.html$/, loader: 'html-loader?attrs[]=video:src' },
+      {
+        test: /\.(jpe?g|png|pdf|gif|woff|woff2|eot|ttf|otf|svg)(\?[a-z0-9=.]+)?$/,
+        loader: "url-loader",
+        options: { limit: 100000 },
+      },
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+        options: {
+          sources: { list: [{ tag: "video", attribute: "src", type: "src" }] },
+        },
+      },
       {
         test: /\.mp4$/,
-        use: 'file-loader?name=videos/[name].[ext]',
+        loader: "file-loader",
+        options: { name: "videos/[name].[ext]" },
       },
-    ]
+    ],
   },
+
   resolve: {
-    extensions: ['*', '.js', '.jsx']
+    extensions: ["*", ".js", ".jsx"],
   },
+
   output: {
-    path: __dirname + '/dist',
-    publicPath: '/',
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    filename: "bundle.js",
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
+
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+
   devServer: {
-    contentBase: './dist',
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
     hot: true,
-    historyApiFallback: true
-  }
+    host: "0.0.0.0",
+    open: true,
+    historyApiFallback: true,
+  },
 };
