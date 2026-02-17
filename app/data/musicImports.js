a35ff79bf3059bb8d9f6dc74b363cfee6bc83945
Abhi Velaga <abhinav.velaga@gmail.com>
2026-02-16 23:48:27 -0600

    music improts webp

--- app/data/musicImports.js
+++ app/data/musicImports.js
@@ -1,55 +1,55 @@
 export const indulgentReleases = [
     {
-        cover: "/images/music/indulgent/rebound.jpg",
+        cover: "/images/music/indulgent/rebound.webp",
         title: "rebound - single",
         year: 2022,
         link: "https://open.spotify.com/album/4L6W0l9LPQhK441vkdfyiN?si=9T00ON31RqyT41o5B9nXzg",
         alt: "rebound album cover" 
     },
     {
-        cover: "/images/music/indulgent/noOneKnows1.jpg",
+        cover: "/images/music/indulgent/noOneKnows1.webp",
         title: "no one knows - single",
         year: 2022,
         link: "https://open.spotify.com/album/6qJYfETeGcK4YTfgqjd74o?si=YHOZ7glASgii2Ys2D5qpMg",
         alt: "no one knows album cover"
     },
     {
-        cover: "/images/music/indulgent/breakMyFall.jpg",
+        cover: "/images/music/indulgent/breakMyFall.webp",
         title: "break my fall - single",
         year: 2022,
         link: "https://open.spotify.com/album/5uWLGQTp9h1zTh2N55NYZR?si=6YBVkmqWReOPhpBXm3lpEw",
         alt: "break my fall album cover"
     },
     {
-        cover: "/images/music/indulgent/noDrive.jpg",
+        cover: "/images/music/indulgent/noDrive.webp",
         title: "no drive will take me home",
         year: 2018,
         link: "https://open.spotify.com/album/2d7c0N7uNdi3jfdsTWeZNV?si=8k7F0AKVRBufzT6zhR4zPg",
         alt: "no drive will take me home album cover" 
     },
     {
-        cover: "/images/music/indulgent/bleachYourHeart.jpg",
+        cover: "/images/music/indulgent/bleachYourHeart.webp",
         title: "bleach your heart - single",
         year: 2018,
         link: "https://open.spotify.com/album/1Bm4qwkp3SGQ4qaF1bmhH6?si=NTVpWxbHS-Cz-2gEkqcG_Q",
         alt: "bleach your heart album cover" 
     },
     {
-        cover: "/images/music/indulgent/pleasureCruise.jpg",
+        cover: "/images/music/indulgent/pleasureCruise.webp",
         title: "pleasure cruise - single",
         year: 2017,
         link: "https://open.spotify.com/album/3sDC6afPmRBgT4PDnGruoB?si=CdLhyM9HSNWq_TelGai5AA",
         alt: "pleasure cruise album cover" 
     },
     {
-        cover: "/images/music/indulgent/toxic.jpg",
+        cover: "/images/music/indulgent/toxic.webp",
         title: "toxic",
         year: 2015,
         link: "https://open.spotify.com/album/67RH1N86fW4hIA10m3DhFl?si=luPqJ3hCTfCNZLzd8IPJSQ",
         alt: "toxic album cover" 
     },
     {
-        cover: "/images/music/indulgent/4am.jpg",
+        cover: "/images/music/indulgent/4am.webp",
         title: "4am - single",
         year: 2015,
         link: "https://open.spotify.com/album/5yPH4IFWFQWJIGM9eiOYPX?si=izE6huAaTPqPOqumHKF70g",
