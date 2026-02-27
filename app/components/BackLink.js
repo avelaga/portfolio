31b60c35afd22a00e8e1239d9c487bc2ba3f300a
Abhi Velaga <abhinav.velaga@gmail.com>
2026-02-26 18:09:42 -0600

    fixed build with types

--- app/components/BackLink.js
+++ app/components/BackLink.js
@@ -2,8 +2,9 @@
 
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
-export default function BackLink() {
+function BackLinkInner() {
   const searchParams = useSearchParams();
   const from = searchParams.get("from");
   const href = from ? `/blog/tagged/${encodeURIComponent(from)}` : "/blog";
@@ -13,5 +14,13 @@ export default function BackLink() {
     <Link href={href} className="back-link">
       &larr; {label}
     </Link>
+  );
+}
+
+export default function BackLink() {
+  return (
+    <Suspense fallback={<span className="back-link">&larr; Back to posts</span>}>
+      <BackLinkInner />
+    </Suspense>
   );
 }
