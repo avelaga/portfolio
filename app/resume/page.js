5a6e380028a37729bf58dcf562772844e4e3501c
Abhi Velaga <abhinav.velaga@gmail.com>
2026-05-16 10:31:32 -0500

    fix resume og

--- app/resume/page.js
+++ app/resume/page.js
@@ -1,6 +1,7 @@
 import Resume from './Resume';
 
 export const metadata = {
+  metadataBase: new URL('https://abhi.work'),
   title: 'Abhi Velaga - Resume',
   description: 'Resume — Abhi Velaga, Senior Frontend Engineer',
   openGraph: {
